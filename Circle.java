b85dc847bd5de7b4d265d17286e025293122adad
dstanton <djstanton@gmail.com>
2021-04-07 14:31:12 -0700

    CylinderExercise
    Udemy Java Master Class

---
+++ Circle.java
@@ -0,0 +1,18 @@
+package cylinder;
+
+public class Circle {
+    private double radius;
+    
+    public Circle(double radius) {
+        if (radius > 0) {
+            this.radius = radius;
+        } else {
+            this.radius = 0;
+        }
+    }
+
+    public double getRadius() {
+        return this.radius;
+    } 
+
+}
---
+++ Cylinder.java
@@ -0,0 +1,6 @@
+package cylinder;
+
+class Cylinder {
+
+}
+
